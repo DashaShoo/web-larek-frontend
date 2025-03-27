48ecdcfd7c44926f81d284db7d9787ea7aede15e
DashaShoo <daryashubabko@gmail.com>
2025-03-27 15:03:33 +0300

    Модальные окна товаров сделала

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import { AppState, ProductItem } from './components/AppData';
 import { Card } from './components/Card';
 import { Page } from './components/Page';
 import { cloneTemplate, createElement, ensureElement } from './utils/utils';
+import {Modal} from "./components/common/Modal";
+import {Form} from "./components/common/Form";
 
 
 const api = new LarekAPI(CDN_URL, API_URL);
@@ -30,6 +32,7 @@ events.onAll(({ eventName, data }) => {
 
 
 const page = new Page(document.body, events);
+const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
 
 
 
@@ -50,6 +53,63 @@ events.on('items:changed', () => {
 	page.counter = appData.basket.length;
 });
 
+events.on('card:select', (item: ProductItem) => {
+	appData.setPreview(item);
+});
+
+events.on('preview:changed', (item: ProductItem) => {
+	const showItem = (item: ProductItem) => {
+		const product = new Card('card', cloneTemplate(cardPreviewTemplate), {
+			onClick: () => events.emit('basket:push', item),
+		});
+
+		modal.render({
+			content: product.render({
+				title: item.title,
+				image: api.cdn + item.image,
+				category: item.category,
+				description: item.description,
+				price: item.price,
+				disabled: !item.price || appData.hasInBasket(item.id) ? true : false,
+			}),
+		});
+	};
+
+	if (item) {
+		api
+			.getProduct(item.id)
+			.then((result) => {
+				item.description = result.description;
+				showItem(item);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	} else {
+		modal.close();
+	}
+});
+
+events.on('modal:open', () => {
+	page.locked = true;
+});
+
+events.on('modal:close', () => {
+	page.locked = false;
+});
+
+
+events.on('basket:push', (item: ProductItem) => {
+	appData.addProduct({
+		id: item.id,
+		index: appData.basket.length,
+		title: item.title,
+		price: item.price,
+	});
+	page.counter = appData.basket.length;
+	modal.close();
+	
+});
 
 
 api.getProductList()
